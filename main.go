9a95b0d54bf4f70514a652cc9617c702e797e778
josiah <josiah@appstack.nyc>
2024-08-07 20:52:56 -0400

    rework how attributes are set and some other thing

--- main.go
+++ main.go
@@ -1,32 +1,51 @@
 package elemx
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type GlobalAttributes struct {
-	Lang string //The lang attribute specifies the language of the element's content.
+func Render(element *HtmlElement) string {
+	return element.toString()
 }
 
-type HtmlAttributes struct {
-	GlobalAttributes
-	Xmls string
-}
-type HtmlElement interface {
-	Render() string
+type HtmlElement struct {
+	Tag        string
+	Content    string
+	Attributes map[string]string
 }
 
-func Render(el HtmlElement) string {
-	return el.Render()
-}
-type Html struct {
-	Content string
-	Attrs *HtmlAttributes
-}
-
-func (h Html) Render() string {
-	template := "<!DOCTYPE html><html>%s</html>"
-	return fmt.Sprintf(template, h.Content)
+func (e *HtmlElement) toString() string {
+	attributes := ""
+	for key, value := range e.Attributes {
+		attributes += fmt.Sprintf(` %s="%s"`, key, value)
+	}
+	return fmt.Sprintf("<%s%s>%s</%s>", e.Tag, attributes, e.Content, e.Tag)
 }
 
+func (e *HtmlElement) SetAttribute(key, value string) {
+	if e.Attributes == nil {
+		e.Attributes = make(map[string]string)
+	}
+	e.Attributes[key] = value
+}
 
+type SetElementAttributes func(*HtmlElement)
+
+func Lang(lang string) SetElementAttributes {
+	return func(e *HtmlElement) {
+		e.SetAttribute("lang", "en")
+	}
+}
+
+// func Html(content string, attr ...SetElementAttributes) string {
+// 	tag := "<html"
+// 	element := HtmlElement{Tag: tag}
+
+// 	for _, set_attr := range attr {
+// 		set_attr(&element)
+// 	}
+
+// 	element.UpdateTag(">")
+// 	fmt.Println(element.Tag)
+// 	return "<html><body>" + content + "</body></html>"
+// }
+
+func Html(content string) *HtmlElement { return &HtmlElement{Tag: "html", Content: content} }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,46 @@
 package elemx
 
 import (
-    "testing"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHtml(t *testing.T) {
-    expected := "<!DOCTYPE html><html></html>"
-    result := Render(Html{Content: ""})
+func TestRender(t *testing.T) {
+	assert := assert.New(t)
 
-    if result != expected {
-        t.Errorf("Html(\"\") = %s; want %s", result, expected)
-    }
+	expected := "<html></html>"
+	
+	result := Render(Html(""))
+
+	assert.Equal(expected, result, "Test Render")
+}
+
+func TestSetAttribute(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := "<html lang=\"en\"></html>"
+	element := HtmlElement{Tag: "html"}
+	element.SetAttribute("lang", "en")
+	result := Render(&element)
+
+	assert.Equal(expected, result, "Test SetAttribute")
+
+	tests:= []struct {
+		description string
+		expected string
+		attribute SetElementAttributes
+	}{
+		{
+			description: "SetAttribute",
+			expected: "<html lang=\"en\"></html>",
+			attribute:  Lang("en"),
+		},
+	}
+
+	for _, test := range tests {
+		element := HtmlElement{Tag: "html"}
+		test.attribute(&element)
+		assert.Equal(test.expected, Render(&element), test.description)
+	}
 }
