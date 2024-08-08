e1119dfc8964b90eaef95ef43714ace730c7a51f
josiah <josiah@appstack.nyc>
2024-08-08 06:39:10 -0400

    include htmx by default

--- main.go
+++ main.go
@@ -39,12 +39,26 @@ func (e *HtmlElement) SetAttribute(key, value string) {
 }
 
 func Lang(lang string) SetAttributes {
-	return func(e *HtmlElement) {
-		e.SetAttribute("lang", "en")
-	}
+	return func(e *HtmlElement) { e.SetAttribute("lang", "en") }
 }
 
-func Html(c string, attrs ...SetAttributes) string { return Render("html", c, attrs) }
-func Body(c string, attrs ...SetAttributes) string { return Render("body", c, attrs) }
-func Div(c string, attrs ...SetAttributes) string  { return Render("div", c, attrs) }
-func Head(c string, attrs ...SetAttributes) string { return Render("head", c, attrs) }
+func Src(src string) SetAttributes {
+	return func(e *HtmlElement) { e.SetAttribute("src", src) }
+}
+
+func ApplyHtmxCDNSource() SetAttributes {
+	return func(e *HtmlElement) { e.SetAttribute("src", "https://unpkg.com/htmx.org@2.0.1") }
+}
+
+func IncludeHtmx() string {
+	return Script("", ApplyHtmxCDNSource())
+}
+
+func Html(c string, attrs ...SetAttributes) string   { return Render("html", c, attrs) }
+func Body(c string, attrs ...SetAttributes) string   { return Render("body", c, attrs) }
+func Div(c string, attrs ...SetAttributes) string    { return Render("div", c, attrs) }
+func Script(c string, attrs ...SetAttributes) string { return Render("script", c, attrs) }
+func Head(c string, attrs ...SetAttributes) string {
+	if len(attrs) == 0 {c = c +IncludeHtmx()}
+	return Render("head", c, attrs)
+}
--- main_test.go
+++ main_test.go
@@ -10,47 +10,70 @@ func TestRender(t *testing.T) {
 	assert := assert.New(t)
 
 	tests := []struct {
-		description     string
-		expected        string
-		renderedElement func() string
+		description string
+		expected    string
+		result      string
 	}{
 		{
-			description:     "Render Empty Element",
-			expected:        "<!DOCTYPE html><html></html>",
-			renderedElement: func() string { return Html("") },
+			description: "Render Empty Element",
+			expected:    "<!DOCTYPE html><html></html>",
+			result:      Html(""),
 		},
 		{
-			description:     "Render Element with Content",
-			expected:        "<div>Hey young world</div>",
-			renderedElement: func() string { return Div("Hey young world") },
+			description: "Render Element with Content",
+			expected:    "<div>Hey young world</div>",
+			result:      Div("Hey young world"),
 		},
 		{
-			description:     "Render Element with Attribute",
-			expected:        "<div lang=\"en\"></div>",
-			renderedElement: func() string { return Div("", Lang("en")) },
+			description: "Render Element with Attribute",
+			expected:    "<div lang=\"en\"></div>",
+			result:      Div("", Lang("en")),
 		},
 		{
-			description:     "Render Nested Element",
-			expected:        "<body><div></div></body>",
-			renderedElement: func() string { return Body(Div("")) },
+			description: "Render Nested Element",
+			expected:    "<body><div></div></body>",
+			result:      Body(Div("")),
 		},
 		{
-			description:     "Render Multiple Elements on the Same Level",
-			expected:        "<div>Hey young world</div><div>The world is yours</div>",
-			renderedElement: func() string { return Div("Hey young world") + Div("The world is yours") },
+			description: "Render Multiple Elements on the Same Level",
+			expected:    "<div>Hey young world</div><div>The world is yours</div>",
+			result:      Div("Hey young world") + Div("The world is yours"),
 		},
 		{
-			description:     "Render Multiple Elements Nested",
-			expected:        "<body><div></div><div></div></body>",
-			renderedElement: func() string { return Body(Div("") + Div("")) },
+			description: "Render Multiple Elements Nested",
+			expected:    "<body><div></div><div></div></body>",
+			result:      Body(Div("") + Div("")),
 		},
 	}
 
 	for _, test := range tests {
-		assert.Equal(test.renderedElement(), test.expected, test.description)
+		assert.Equal(test.expected, test.result, test.description)
 	}
 }
+func TestHtmxCompatibility(t *testing.T) {
+	assert := assert.New(t)
 
+	tests := []struct {
+		description string
+		expected    string
+		rendered    string
+	}{
+		{
+			description: "Include htmx",
+			expected:    "<script src=\"https://unpkg.com/htmx.org@2.0.1\"></script>",
+			rendered:    Script("", ApplyHtmxCDNSource()),
+		},
+		{
+			description: "Empty Head Tag default include htmx", // ? not sure if this is correct at the moment
+			expected:    "<head><script src=\"https://unpkg.com/htmx.org@2.0.1\"></script></head>",
+			rendered:    Head(""),
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(test.expected, test.rendered, test.description)
+	}
+}
 func TestSetAttribute(t *testing.T) {
 	assert := assert.New(t)
 
